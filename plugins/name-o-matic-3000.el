d5e29df4e9ddcad64aec255903d90fb02f3ecba5
Sune Hellfritzsch <sunehellfritzsch@gmail.com>
2020-09-19 13:35:25 +0200

    Simplify

--- plugins/name-o-matic-3000.el
+++ plugins/name-o-matic-3000.el
@@ -8,27 +8,7 @@
    (shell-command-to-string
     (concat "~/name-o-matic-3000/name-o-matic-3000.py" " " type))))
 
-(defun insert-namespace-name ()
-  "Generate and insert a random namespace name in the current buffer at point."
-  (interactive)
-  (insert-name "namespace"))
-
-(defun insert-class-name ()
-  "Generate and insert a random class name in the current buffer at point."
-  (interactive)
-  (insert-name "class"))
-
-(defun insert-function-name ()
-  "Generate and insert a random function name in the current buffer at point."
-  (interactive)
-  (insert-name "function"))
-
-(defun insert-variable-name ()
-  "Generate and insert a random variable name in the current buffer at point."
-  (interactive)
-  (insert-name "variable"))
-
-(global-set-key (kbd "<f1>") 'insert-namespace-name)
-(global-set-key (kbd "<f2>") 'insert-class-name)
-(global-set-key (kbd "<f3>") 'insert-function-name)
-(global-set-key (kbd "<f4>") 'insert-variable-name)
+(global-set-key (kbd "<f1>") '(lambda () (interactive) (insert-name "namespace")))
+(global-set-key (kbd "<f2>") '(lambda () (interactive) (insert-name "class")))
+(global-set-key (kbd "<f3>") '(lambda () (interactive) (insert-name "function")))
+(global-set-key (kbd "<f4>") '(lambda () (interactive) (insert-name "variable")))
